8e6c7f7288c1c2c4371254fc2c2506b563b245ba
manuel1358000 <manuel13580@gmail.com>
2020-06-26 09:40:58 -0600

    configuraciones servidor

--- servidor/src/conexiondb/conexion.js
+++ servidor/src/conexiondb/conexion.js
@@ -2,11 +2,11 @@ var mysql = require('mysql');
 const config = require('../../config/authdb.key');
  var mysql_pool  = mysql.createPool({
     connectionLimit : 100,
-    host            : 'localhost',
+    host            : '192.168.1.3',
     user            : config.user,
     password        : config.password,
     database        : 'db_rutasheca',
-    port            :3306   
+    port            :3307   
   });
 
  module.exports.mysql_pool=mysql_pool;
